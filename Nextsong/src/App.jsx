82dc9f2f2045367973c702e5da1d953b6b0b6cc0
Vladimir-28 <20243ds028@utez.edu.mx>
2026-03-11 21:08:11 -0600

    borrado de importaciones en archivo app

--- Nextsong/src/App.jsx
+++ Nextsong/src/App.jsx
@@ -1,15 +1,11 @@
 import { useState } from 'react'
 import PublicRouter from './modules/router/PublicRoter'
-import Songs from './modules/auth/songs/views/Songs'
-import Events from './modules/auth/events/views/Events'
-import Home from './modules/auth/home/Home'
 
 function App() {
 
   return (
     
-      <Home/>
-    
+    <></>
   )
 }
 
