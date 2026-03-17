62aa23611e3aecf8f1f7376b3fb85ce2bdaeae51
Vladimir-28 <20243ds028@utez.edu.mx>
2026-03-17 04:21:06 -0600

    pantalla de canciones de evento

--- Nextsong/src/modules/auth/events/components/SongEventCard.jsx
+++ Nextsong/src/modules/auth/events/components/SongEventCard.jsx
@@ -2,13 +2,16 @@ import { FiTag } from "react-icons/fi";
 import { FaRegClock } from "react-icons/fa6";
 
 export default function SongEventCard({ songEvent }) {
+
+    const song = songEvent.song;
+
     return (
 
-        <div key={songEvent.id} className="card p-3 mb-2 shadow-sm">
+        <div className="card p-3 mb-2 shadow-sm">
 
             <div className="d-flex align-items-center">
 
-                {/* Número */}
+                {/* Orden */}
                 <div
                     className="d-flex justify-content-center align-items-center me-3"
                     style={{
@@ -20,30 +23,30 @@ export default function SongEventCard({ songEvent }) {
                         fontSize: "20px"
                     }}
                 >
-                    {songEvent.id}
+                    {songEvent.songOrder}
                 </div>
 
                 {/* Contenido */}
                 <div className="flex-grow-1">
 
                     <strong className="d-block">
-                        {songEvent.title}
+                        {song.title}
                     </strong>
 
                     <small className="text-muted">
-                        {songEvent.artist}
+                        {song.author}
                     </small>
 
                     <div className="d-flex gap-2 mt-2">
 
                         <span className="badge bg-light text-muted d-flex align-items-center">
                             <FaRegClock size={14} className="me-1"/>
-                            {songEvent.bpm} BPM
+                            {song.bpm} BPM
                         </span>
 
                         <span className="badge bg-light text-muted d-flex align-items-center">
                             <FiTag size={14} className="me-1"/>
-                            {songEvent.tone}
+                            {song.keyTone}
                         </span>
 
                     </div>
