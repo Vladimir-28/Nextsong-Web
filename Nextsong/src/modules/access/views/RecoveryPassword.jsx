f7b8e355c4d0d2f1223f3ec1f8923887d651836c
Katya-hd <20243ds018@utez.edu.mx>
2026-04-09 22:02:10 -0600

    Implementacion del recuperar contraseña y validaciones especificas de las contraseñas

--- Nextsong/src/modules/access/views/RecoveryPassword.jsx
+++ Nextsong/src/modules/access/views/RecoveryPassword.jsx
@@ -1,13 +1,46 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
-import Header from "../components/Header";
+import { useNavigate } from "react-router-dom";
 
 export default function Recovery() {
+
+    const [email, setEmail] = useState("");
+    const [message, setMessage] = useState("");
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+     
+
+        try {
+            const response = await fetch(
+                `http://localhost:8080/auth/recover-password?email=${email}`,
+                {
+                    method: "POST"
+                }
+            );
+
+         if (response.ok) {
+            setMessage("Código enviado ");
+            setTimeout(() => {
+                navigate("/verify-code", { state: { email } });
+            }, 1500);
+        } else {
+                const error = await response.text();
+                setMessage(error);
+            }
+
+        } catch (error) {
+            setMessage("Error al conectar con el servidor");
+        }
+    };
+
+
 
     return (
         <main className="d-flex flex-column align-items-center">
 
-           
-            {/* RECOVERY */}
             <div
                 className="d-flex justify-content-center align-items-center w-100"
                 style={{ minHeight: "80vh" }}
@@ -15,68 +48,52 @@ export default function Recovery() {
                 <div className="card border-0 shadow" style={{ width: 450 }}>
                     <div className="card-body p-4">
 
-                        {/* Volver */}
                         <Link
                             to="/login"
                             className="text-decoration-none d-inline-block mb-3 text-secondary"
                         >
-                            <i className="bi bi-arrow-left me-2"></i>
-                            Volver a inicio de sesión
+                            ← Volver
                         </Link>
 
                         <div className="text-center mb-4">
                             <h2 className="fw-bold">Recuperar contraseña</h2>
                             <p className="text-muted mb-0">
-                                Ingrese su correo electrónico y le enviaremos un enlace
-                                para restablecer su contraseña
+                                Ingrese su correo y le enviaremos un código de 6 dígitos.
                             </p>
                         </div>
-                        
 
-                        <form className="row g-3">
+                        <form className="row g-3" onSubmit={handleSubmit}>
 
-                            {/* EMAIL */}
                             <div className="col-12">
                                 <label className="form-label">
                                     Correo electrónico *
                                 </label>
 
-                                <div className="input-group">
-                                    <span className="input-group-text">
-                                        <i className="bi bi-envelope"></i>
-                                    </span>
-
-                                    <input
-                                        type="email"
-                                        className="form-control"
-                                        placeholder="tu@email.com"
-                                    />
-                                </div>
+                                <input
+                                    type="email"
+                                    className="form-control"
+                                    placeholder="tu@email.com"
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    required
+                                />
                             </div>
 
-                            {/* BOTON */}
                             <div className="col-12 mt-3">
                                 <button
+                                    type="submit"
                                     className="btn w-100 text-white"
                                     style={{ backgroundColor: "#a56d49" }}
                                 >
-                                    Enviar enlace de recuperación
+                                    Enviar código
                                 </button>
                             </div>
 
-                            <div className="col-12 text-center mt-3">
-                                <span className="text-muted">
-                                    ¿Recordó su contraseña?
-                                </span>
-
-                                <Link
-                                    className="ms-2"
-                                    to="/login"
-                                    style={{ color: "#b97a56" }}
-                                >
-                                    Inicie sesión
-                                </Link>
-                            </div>
+                            {message && (
+                                <div className="col-12 text-center mt-3">
+                                    <small>{message}</small>
+                                </div>
+                            )}
 
                         </form>
 
